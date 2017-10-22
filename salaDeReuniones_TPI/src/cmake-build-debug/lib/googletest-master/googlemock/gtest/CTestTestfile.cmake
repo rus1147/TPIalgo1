7ec9db6cb999166bbb50e8827165c3ccf134ef45
rus1147 <rus1147@gmail.com>
2017-10-22 16:34:25 -0300

    "borro archivos de debug"

--- salaDeReuniones_TPI/src/cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ruslan/Facultad/Algo1 2017/TPI2017/TPIalgo1/salaDeReuniones_TPI/src/lib/googletest-master/googletest
-# Build directory: /home/ruslan/Facultad/Algo1 2017/TPI2017/TPIalgo1/salaDeReuniones_TPI/src/cmake-build-debug/lib/googletest-master/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
